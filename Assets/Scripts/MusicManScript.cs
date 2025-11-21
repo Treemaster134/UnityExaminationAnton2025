64d36405b41d575552bffadc13e960f1243356f5
Anton Lamberth <a25antla@student.his.se>
2025-11-21 16:19:54 +0100

    Tog bort onödiga saker som inte längre används

--- Assets/Scripts/MusicManScript.cs
+++ Assets/Scripts/MusicManScript.cs
@@ -5,42 +5,13 @@ using UnityEngine.InputSystem;
 public class MusicManScript : MonoBehaviour
 {
     [SerializeField] private GameObject text;
-    [SerializeField] private GameObject[] dialogGameObjects;
-    private bool touchingPlayer = false;
-    private int dialogIndex = 0;
-    private bool pressedInteract = false;
-    private void Update()
-    {
-        if (touchingPlayer && pressedInteract)
-        {
-            dialogGameObjects[dialogIndex].SetActive(true);
-            dialogGameObjects[dialogIndex].GetComponent<Dialog>().StartDialog();
-
-            if (dialogIndex < (dialogGameObjects.Length - 1))
-            {
-                dialogIndex++;
-            }
-            
-            text.gameObject.SetActive(false);
-            touchingPlayer = false;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         text.gameObject.SetActive(true);
-        touchingPlayer = true;
     }
     
     private void OnTriggerExit2D(Collider2D other)
     {
         text.gameObject.SetActive(false);
-        touchingPlayer = false;
-    }
-
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if(context.started) pressedInteract = true;
-        if(context.canceled) pressedInteract = false;
     }
 }
--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -5,25 +5,6 @@ using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
-    [SerializeField] private AudioMixer mixer;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        if(PlayerPrefs.HasKey("Volume"))
-        {
-            print(PlayerPrefs.GetFloat("Volume"));
-            SetVolume(PlayerPrefs.GetFloat("Volume"));
-        }
-    }
-
-    public void SetVolume(float v)
-    {
-        //Explanation: https://discussions.unity.com/t/changing-audio-mixer-group-volume-with-ui-slider/567394/12
-        //convert the linear value of the slider to a logarithmic one for the mixer 
-        mixer.SetFloat("Volume", Mathf.Log(v) * 20);
-        PlayerPrefs.SetFloat("Volume", v);
-    }
-
     public void EnterScene(string scene)
     {
         SceneManager.LoadScene(scene);
